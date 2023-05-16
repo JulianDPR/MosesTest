8b703866f532c348109c11613b39482e4dea6f69
JulianDPR <penaranda.julian@correounivalle.edu.co>
2023-05-16 17:13:59 -0500

    v0

---
+++ main.py
@@ -0,0 +1,65 @@
+import pandas as pd
+import numpy as np
+import scipy.stats as ss
+
+
+def MosesTest(x,y,extreme=None):
+
+    def factorial(x):
+
+	    y = 1
+	    for i in range(1,x+1):
+		    y = y*i
+	    return y
+
+    def combinatoria(x,y,factorial=factorial):
+
+	    z = factorial(x)/(factorial(y)*factorial(x-y))
+	    return z
+
+    def moses(r,m,h,n, combinatoria=combinatoria):
+
+	    y = combinatoria(r+m-2*h-2, r)
+	    x = combinatoria(n+2*h+1-r, n-r)
+	    z = combinatoria(m+n, m)
+        return y*x/z
+
+    def cdf(r, m, h, n, moses=moses):
+
+        r = list(range(r+1))
+        return np.sum(list(map(lambda x: moses(x, m, h, n), r)))
+
+    m = len(x)
+    n = len(y)
+
+    f = pd.DataFrame(x+y, columns=["value"])
+    f["is"]=np.select([f.index<len(x), f.index>=len(x)], ["x", "y"])
+    f = f.sort_values("value")
+    f.index = list(range(1, len(f)+1))
+
+    indexer = np.cumsum(f["is"]=="x")<=extreme
+    f.loc[indexer, "is"]="h"
+    indexer = np.cumsum((f["is"]=="x")[::-1])<=extreme
+    f.loc[indexer,"is"]="h"
+
+    inf = f.index[f["is"]=="x"][0]
+    sup = f.index[f["is"]=="x"][-1]
+    s_ = sup-inf+1
+
+    if extreme != None and type(extreme)!=type(str()):
+        m_h = len(x)-2*extreme
+    elif type(extreme)!=type(str()):
+        m_h = len(x)-0
+
+    r = s_ - m_h
+    valor_p = cdf(r, m, extreme, n)
+
+    return [s_, valor_p]
+
+
+#x = [21,21.4,22.5,23.6,25.8,25.9,27,27.8,30.1,30.5,33.7,34.8,35.1,35.3,38.9,45]
+#y = [17.4,18.6,19.1,22.3,24.7,25.2,26.1,27.1,28,29.5,30.3,30.4]
+
+#print(MosesTest(x,y,4))
+
+
